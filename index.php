e75a1d3937b342ce52558052a4f91afdb59a6f88
KGibbs81 <126337666+KGibbs81@users.noreply.github.com>
2024-09-15 15:37:48 +0100

    First Updated files with a working PHP Script
    Simply returns whatever is written into th the input text box

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>TEST Azure PHP</title>
+</head>
 
-echo "Kev has made a PHP site in Azure";
+<body>
+
+<h2>Simple PHP Script that will return whatever you write into the text box!</h2><br>
+
+<form id="form" action="result.php" method="post">
+
+<input type="text" id="input" name="input">
+<input type="submit" name="submit">
+
+
+
+</form>
+
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Result</title>
+</head>
+
+<body>
+<h1>Result:</h1><br>
+
+<?php
+
+	$input = $_POST ["input"];
+
+echo $input;
+?>
+
+</body>
+</html>
